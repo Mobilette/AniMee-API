8cadd4a80bb59162ddcf2f2b7080dc3c61b85ce5
philippe beroucry <philippe.beroucry@philippes-MacBook-Pro.local>
2016-02-14 17:25:14 +0100

    Scenario 1 + Create anislitService/animeeController/EpisodeController

--- app.js
+++ app.js
@@ -1,69 +1,22 @@
-'use strict';
-
-/*
- * Express Dependencies
- */
+'uses strict';
 var express = require('express');
+var animeController = require('./controllers/anime');
+var bodyParser = require('body-parser');
+var port = process.env.PORT || 3000;
 var app = express();
-var port = 3000;
 
-/*
- * Use Handlebars for templating
- */
-var exphbs = require('express3-handlebars');
-var hbs;
-
-// For gzip compression
-app.use(express.compress());
-
-/*
- * Config for Production and Development
- */
-if (process.env.NODE_ENV === 'production') {
-    // Set the default layout and locate layouts and partials
-    app.engine('handlebars', exphbs({
-        defaultLayout: 'main',
-        layoutsDir: 'dist/views/layouts/',
-        partialsDir: 'dist/views/partials/'
-    }));
-
-    // Locate the views
-    app.set('views', __dirname + '/dist/views');
-    
-    // Locate the assets
-    app.use(express.static(__dirname + '/dist/assets'));
-
-} else {
-    app.engine('handlebars', exphbs({
-        // Default Layout and locate layouts and partials
-        defaultLayout: 'main',
-        layoutsDir: 'views/layouts/',
-        partialsDir: 'views/partials/'
-    }));
-
-    // Locate the views
-    app.set('views', __dirname + '/views');
-    
-    // Locate the assets
-    app.use(express.static(__dirname + '/assets'));
-}
-
-// Set Handlebars
-app.set('view engine', 'handlebars');
-
-
+// Use the body-parser package in our application
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 /*
  * Routes
  */
 // Index Page
-app.get('/', function(request, response, next) {
-    response.render('index');
+app.get('/', function(req, res, next) {
+    res.json({message: 'connection api Animee ok' });
 });
 
-
-/*
- * Start it up
- */
-app.listen(process.env.PORT || port);
+app.get("/api/anime/from/today", animeController.getAnimeReleaseFromToday);
+app.listen(port);
 console.log('Express started on port ' + port);
